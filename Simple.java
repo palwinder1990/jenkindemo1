6e976c869d6ce17a328266f82ec87e432f6ff0a5
palwinder1990 <98020598+palwinder1990@users.noreply.github.com>
2022-04-07 10:21:00 +0530

    update date07/04

--- Simple.java
+++ Simple.java
@@ -2,7 +2,7 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java"); 
       System.out.println("Hello Java Updated"); 
-         System.out.println("Today date is 06-04-2022 and time is 18:35pm"); 
+         System.out.println("Today date is 07-04-2022 and time is 18:35pm"); 
          System.out.println("Today we will learn Jenkin job");
     }  
 }
